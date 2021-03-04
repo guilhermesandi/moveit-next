0af3f5e8fe34cafe4f8fcfaa59c267c3702130f4
Guilherme Sandi <guilherme_sandi@hotmail.com>
2021-03-04 19:41:53 -0300

    ExperienceBar and CompleteChallenges

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -10,6 +10,7 @@ interface Challenge {
 interface ChallengesContextData {
   level: number;
   currentExperience: number;
+  experienceToNextLevel: number;
   challengesCompleted: number;
   activeChallenge: Challenge;
   levelUp: () => void;
@@ -30,6 +31,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
   const [activeChallenge, setActiveChallenge] = useState(null);
 
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
   function levelUp() {
     setLevel(level + 1);
   }
@@ -49,6 +52,7 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     <ChallengesContext.Provider value={{
       level,
       currentExperience,
+      experienceToNextLevel,
       challengesCompleted,
       levelUp,
       startNewChallenge,
